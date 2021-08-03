18a651d7e4868d55e8ce02facb19d34a91a18acb
VictorHSilva <victorhugo_rosa@hotmail.com>
2021-08-02 21:36:10 -0300

    styled-components

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyle } from './styles/global-styles';
 import Home from './templates/App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}></ThemeProvider>
+    <GlobalStyle />
     <Home />
   </React.StrictMode>,
   document.getElementById('root'),
